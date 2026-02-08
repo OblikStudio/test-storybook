7f7d949d544eff5d90724adbef94d94ca3a0c552
Hristiyan Dodov <h.dodov@gmail.com>
2026-02-08 19:47:01 +0200

    Add reproduction example

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,4 +3,17 @@ import { definePreview } from "@storybook/nextjs";
 
 export default definePreview({
   addons: [addonDocs()],
+  parameters: {
+    layout: "padded",
+    // ^?
+  },
+  decorators: [
+    (_, context) => {
+      const params = context.parameters;
+      //      ^?
+      const layout = context.parameters.layout;
+      //      ^?
+      return <></>;
+    },
+  ],
 });
